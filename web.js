0c16621c9228ac783599c8f97e7fef74308cc240
Nitin Gupta <ngupta.ms@gmail.com>
2013-09-18 13:33:05 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+var buf = fs.readFileSync('index.html');
+var str = buf.toString();
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
